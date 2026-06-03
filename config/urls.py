941ff7ae5049bcaf63a839af42fa16e718cddc2d
mokhtar-web <belbalmokhtar@gmail.com>
2026-06-03 07:45:30 +0100

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -2,31 +2,18 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import RedirectView
-from properties.views import home  # استيراد home من views الخاصة بالعقارات
-from accounts import views as accounts_views
-from django.conf import settings
-from django.conf.urls.static import static
+from properties import views  # استيراد views من تطبيق properties
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    # هذا هو المسار الذي يحتاجه القالب
+    path('developers/', views.developer_list, name='developer_list'),
     path('accounts/', include('accounts.urls')),
     path('properties/', include('properties.urls')),
     path('rewards/', include('rewards.urls')),
-    path('properties/', include('properties.urls', namespace='properties')),
-    path('', home, name='home'),  # الصفحة الرئيسية
     path('reports/', include('reports.urls')),
-    path('developers/', views.developer_list, name='developer_list'),
-
-    # تحويل المسار القديم /developers/ إلى المسار الجديد /properties/promoteurs/
-    path('developers/', RedirectView.as_view(url='/properties/promoteurs/', permanent=True),
-         name='developer_list_global'),
-
 ]
 
-if settings.DEBUG:  # ← بدون مسافات إضافية، محاذي تماماً لليسار
+if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-
-
